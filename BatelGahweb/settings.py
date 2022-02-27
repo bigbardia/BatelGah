92b0b9a20aa185953e7c29bca33af8aaa0633152
bigbardia <bigbardia@gmail.com>
2022-02-27 18:14:48 +0330

    static files

--- BatelGahweb/settings.py
+++ BatelGahweb/settings.py
@@ -112,6 +112,7 @@ TIME_ZONE = "Iran"
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
